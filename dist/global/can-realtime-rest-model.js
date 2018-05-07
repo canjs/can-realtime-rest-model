8ee6973a944acd9f5e0bdcaa6e9a85da1cdf906f
Justin Meyer <justinbmeyer@gmail.com>
2018-05-07 16:50:37 -0500

    0.1.1

---
+++ dist/global/can-realtime-rest-model.js
@@ -0,0 +1,181 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval) {
+	// jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name) {
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for (i = 0; i < parts.length; i++) {
+			if (!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val) {
+		var parts = name.split("."),
+			cur = global,
+			i,
+			part,
+			next;
+		for (i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if (!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod) {
+		if (!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, default: true };
+		for (var p in mod) {
+			if (!esProps[p]) return false;
+		}
+		return true;
+	};
+
+	var hasCjsDependencies = function(deps) {
+		return (
+			deps[0] === "require" && deps[1] === "exports" && deps[2] === "module"
+		);
+	};
+
+	var modules =
+		(global.define && global.define.modules) ||
+		(global._define && global._define.modules) ||
+		{};
+	var ourDefine = (global.define = function(moduleName, deps, callback) {
+		var module;
+		if (typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for (i = 0; i < deps.length; i++) {
+			args.push(
+				exports[deps[i]]
+					? get(exports[deps[i]])
+					: modules[deps[i]] || get(deps[i])
+			);
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if (hasCjsDependencies(deps) || (!deps.length && callback.length)) {
+			module = { exports: {} };
+			args[0] = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args[1] = module.exports;
+			args[2] = module;
+		} else if (!args[0] && deps[0] === "exports") {
+			// Babel uses the exports and module object.
+			module = { exports: {} };
+			args[0] = module.exports;
+			if (deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if (!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if (globalExport && !get(globalExport)) {
+			if (useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	});
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function() {
+		// shim for @@global-helpers
+		var noop = function() {};
+		return {
+			get: function() {
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load) {
+				doEval(__load.source, global);
+			}
+		};
+	});
+})(
+	{},
+	typeof self == "object" && self.Object == Object
+		? self
+		: typeof process === "object" &&
+		  Object.prototype.toString.call(process) === "[object process]"
+			? global
+			: window,
+	function(__$source__, __$global__) {
+		// jshint ignore:line
+		eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+	}
+);
+
+/*can-realtime-rest-model@0.1.0#can-realtime-rest-model*/
+define('can-realtime-rest-model', [
+    'require',
+    'exports',
+    'module',
+    'can-connect',
+    'can-connect/constructor/constructor',
+    'can-connect/can/map/map',
+    'can-connect/constructor/store/store',
+    'can-connect/data/callbacks/callbacks',
+    'can-connect/data/parse/parse',
+    'can-connect/data/url/url',
+    'can-connect/real-time/real-time',
+    'can-connect/constructor/callbacks-once/callbacks-once',
+    'can-namespace'
+], function (require, exports, module) {
+    var connect = require('can-connect');
+    var constructor = require('can-connect/constructor/constructor');
+    var canMap = require('can-connect/can/map/map');
+    var constructorStore = require('can-connect/constructor/store/store');
+    var dataCallbacks = require('can-connect/data/callbacks/callbacks');
+    var dataParse = require('can-connect/data/parse/parse');
+    var dataUrl = require('can-connect/data/url/url');
+    var realTime = require('can-connect/real-time/real-time');
+    var callbacksOnce = require('can-connect/constructor/callbacks-once/callbacks-once');
+    var namespace = require('can-namespace');
+    function realtimeRestModel(options) {
+        var behaviors = [
+            constructor,
+            canMap,
+            constructorStore,
+            dataCallbacks,
+            dataParse,
+            dataUrl,
+            realTime,
+            callbacksOnce
+        ];
+        return connect(behaviors, options);
+    }
+    module.exports = namespace.realtimeRestModel = realtimeRestModel;
+});
+/*[global-shim-end]*/
+(function(global) { // jshint ignore:line
+	global._define = global.define;
+	global.define = global.define.orig;
+}
+)(typeof self == "object" && self.Object == Object ? self : (typeof process === "object" && Object.prototype.toString.call(process) === "[object process]") ? global : window);
