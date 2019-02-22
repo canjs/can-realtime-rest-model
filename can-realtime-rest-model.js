9c655bb772e3d61eacde62846497b214abfa0789
Chasen Le Hara <git@chasenlehara.com>
2019-02-22 09:48:41 -0800

    Add a new signature (just pass in a string) and use can-define to set up List and Map if they’re not passed in
    This makes the following possible:
    
    ```js
    const Todo = realtimeRestModel("/api/todos/{id}").Map;
    ```
    
    - Pass in just a string to `realtimeRestModel()` to configure the connection
    - If `Map` or `List` aren’t defined in the options, then `can-define` is used by default
    
    Closes https://github.com/canjs/can-realtime-rest-model/issues/4

--- can-realtime-rest-model.js
+++ can-realtime-rest-model.js
@@ -6,11 +6,24 @@ var constructorStore = require("can-connect/constructor/store/store");
 var dataCallbacks = require("can-connect/data/callbacks/callbacks");
 var dataParse = require("can-connect/data/parse/parse");
 var dataUrl = require("can-connect/data/url/url");
+var DefineList = require("can-define/list/list");
+var DefineMap = require("can-define/map/map");
 var realTime = require("can-connect/real-time/real-time");
 var callbacksOnce = require("can-connect/constructor/callbacks-once/callbacks-once");
 var namespace = require("can-namespace");
 
-function realtimeRestModel(options){
+function realtimeRestModel(optionsOrUrl) {
+
+	// If optionsOrUrl is a string, make options = {url: optionsOrUrl}
+	var options = (typeof optionsOrUrl === "string") ? {url: optionsOrUrl} : optionsOrUrl;
+
+	// If options.Map or .List aren’t provided, define them
+	if (typeof options.Map === "undefined") {
+		options.Map = DefineMap.extend({seal: false}, {});
+	}
+	if (typeof options.List === "undefined") {
+		options.List = options.Map.List || DefineList.extend({"#": options.Map});
+	}
 
 	var behaviors = [
 		constructor,
